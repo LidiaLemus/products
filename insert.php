daf2880ab27cf2195e3ec4b5de04176280814296
LidiaLemus <lidiaagustinesq1997@gmail.com>
2019-05-14 15:58:04 -0600

    cambios en el insert

--- insert.php
+++ insert.php
@@ -16,7 +16,7 @@ $pass = $_POST["pass"];
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO products VALUES (null, '$productname', '$pass')";
+    $sql = "INSERT INTO products VALUES (null, '$product_name', '$price','$is_activate')";
     $conn->exec($sql);
     echo "New record created successfully";
     }
