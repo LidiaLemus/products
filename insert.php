f53169f145be6c8c0b645ea0787a8aa188176ca6
LidiaLemus <lidiaagustinesq1997@gmail.com>
2019-05-14 15:54:21 -0600

    insert.php

---
+++ insert.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345";
+$dbname = "auth";
+
+$user = $_POST["user"];
+$pass = $_POST["pass"];
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO products VALUES (null, '$productname', '$pass')";
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+$conn = null;
+?> 
+</body>
+</html>
