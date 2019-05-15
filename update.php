789567c8c488ad69b04c5b4f24e3724dcdbca455
LidiaLemus <lidiaagustinesq1997@gmail.com>
2019-05-15 14:07:11 -0600

    cambios en identacion

--- update.php
+++ update.php
@@ -1,1 +1,24 @@
-update.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+ <?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "12345";
+$dbname = "products";
+ 
+ try(){
+ 	
+
+ }catch(PDOException $e){
+ 	 echo $sql . "<br>" . $e->getMessage();
+ }
+$conn = null;
+   ?>
+</body>
+</html>
